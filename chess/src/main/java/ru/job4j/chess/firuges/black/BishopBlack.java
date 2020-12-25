0e3e5f340c8fdc170ed0b92459cf813bb9b100a5
ikioresko <ikioresko@gmail.com>
2020-12-25 11:05:13 +0500

    2. Каркас шахматной доски [#418341]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -27,8 +27,12 @@ public class BishopBlack implements Figure {
         Cell[] steps = new Cell[size];
         int deltaX = (position.getX() > dest.getX()) ? -1 : 1;
         int deltaY = (position.getY() > dest.getY()) ? -1 : 1;
-        for (int index = 1; index <= size; index++) {
-            steps[index-1] = Cell.findBy( position.getX() + deltaX * index, position.getY() + deltaY * index);
+        int x = position.getX();
+        int y = position.getY();
+        for (int index = 0; index < size; index++) {
+            x += deltaX;
+            y += deltaY;
+            steps[index] = Cell.findBy(x, y);
         }
         return steps;
     }
